92fae7ccef903b798dff9afb4485b71ab3eb7452
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-05-17 18:02:19 +0600

    Add files via upload

---
+++ CSE115_LAB/Class_performance_4.c
@@ -0,0 +1,20 @@
+//Barshon Basak    ID : 2311625642
+
+#include <stdio.h>
+
+void grape (void);
+
+int main()
+{
+    grape();
+    return 0;
+}
+
+void grape (void)
+{
+     printf("   \\\n");
+     printf("  ()()\n");
+     printf(" ()()()\n");
+     printf("  ()()\n");
+     printf("   ()\n");
+}
