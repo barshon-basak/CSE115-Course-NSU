807b1c3c62dc0c32dd74de824c064ee1ee983731
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-05-17 18:04:11 +0600

    Add files via upload

---
+++ CSE115_LAB/Class_performance_6.c
@@ -0,0 +1,19 @@
+//Barshon Basak    ID: 2311625642
+
+#include<stdio.h>
+
+int main(void){
+
+int num;
+
+printf("enter a number: \n");
+scanf("%d",&num);
+
+if(num%2==0){
+    printf("This is a even number");
+}
+else{
+    printf("This is a odd number");
+}
+return 0;
+}
